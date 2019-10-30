31b5b01ec4830f3e9e4b85a1273178e9d98fa543
Erin Kahn <erinnicolekahn@gmail.com>
2019-10-30 14:48:03 -0400

    Initial commit

---
+++ page-plants.php
@@ -0,0 +1,38 @@
+
+<?php
+/*
+* Template Name: Plants Page
+*/
+?>
+
+<?php get_header();?>
+
+
+
+
+<h2>Plants</h2>
+
+<?php
+if (have_posts()):
+    while (have_posts() ):
+        the_post();
+        echo '<li>';
+            echo '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a>';
+            echo ' by ' . '<em>' . get_the_author() . '</em>';
+        echo '</li>';
+    endwhile;
+endif;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer();?>
